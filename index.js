f786c85473c19a05b468c4a002f235d47fc5890d
NRenukeshwar <noolurenu.97@gmail.com>
2020-04-27 23:12:21 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      todolist: [{ priority: "3", text: "Sample Task 3" },{ priority: "1", text: "Sample Task 1" }],
+      todolist: [
+        { priority: "3", text: "Sample Task 3" },
+        { priority: "1", text: "Sample Task 1" }
+      ],
       text: "",
       priority: ""
     };
@@ -30,14 +33,14 @@ class App extends Component {
         todolist.push(todo);
         this.setState({
           todolist: todolist,
-          text:"",
-          priority:""
+          text: "",
+          priority: ""
         });
       } else {
         alert("Todo item already added");
         this.setState({
-          text:"",
-          priority:""
+          text: "",
+          priority: ""
         });
       }
     }
@@ -63,6 +66,7 @@ class App extends Component {
       todolist: list
     });
   };
+  
   render() {
     const sorted = this.state.todolist.sort((a, b) =>
       a.priority.localeCompare(b.priority)
@@ -79,50 +83,50 @@ class App extends Component {
       <div>
         <h1 className="text-white text-center bg-primary mb-3">TODO APP</h1>
         <div className="container">
-        <form
-          className="m-2"
-          onSubmit={event => this.handleSubmit(event)}
-          method="POST"
-        >
-          <div className="row">
-            <input
-              className="form-control offset-sm-1 col-sm-7"
-              type="text"
-              id="text"
-              name="text"
-              placeholder="Enter Todo item text"
-              value={this.state.text}
-              onChange={event => this.handleChange(event)}
-              required
-            />
+          <form
+            className="m-2"
+            onSubmit={event => this.handleSubmit(event)}
+            method="POST"
+          >
+            <div className="row">
+              <input
+                className="form-control offset-sm-1 col-sm-7"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Enter Todo item text"
+                value={this.state.text}
+                onChange={event => this.handleChange(event)}
+                required
+              />
 
-            <select
-              id="priority"
-              name="priority"
-              className="form-control col-sm-2"
-              onChange={event => this.handleChange(event)}
-              value={this.state.priority}
-              required
-            >
-              <option value="">Select Priority</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-            <div className="col d-flex justify-content-center">
-              <button id="submit" className="btn btn-success">
-                Add ToDo
-              </button>
+              <select
+                id="priority"
+                name="priority"
+                className="form-control col-sm-2"
+                onChange={event => this.handleChange(event)}
+                value={this.state.priority}
+                required
+              >
+                <option value="">Select Priority</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <div className="col d-flex justify-content-center">
+                <button id="submit" className="btn btn-success">
+                  Add ToDo
+                </button>
+              </div>
             </div>
+          </form>
+          <p className="text-center mt-3">
+            <i>ToDo Items pending list...</i>
+          </p>
+          <div style={{ maxWidth: "700px" }} className="mx-auto">
+            {show}
           </div>
-        </form>
-        <p className="text-center mt-3">
-          <i>ToDo Items pending list...</i>
-        </p>
-        <div style={{ maxWidth: "700px" }} className="mx-auto">
-          {show}
         </div>
-      </div>
       </div>
     );
   }
